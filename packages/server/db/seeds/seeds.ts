16d64271e1df30c4e324c244defc1f9b8f50783a
Ketut Sandiarsa <ktutnik@gmail.com>
2019-02-16 08:36:30 +0800

    Add callback

--- packages/server/db/seeds/seeds.ts
+++ packages/server/db/seeds/seeds.ts
@@ -1,10 +1,11 @@
 import * as Knex from "knex";
 
 export function seed(knex: Knex): Promise<any> {
-    async function addIfNotExists<T>(table: string, criteria: Partial<T>, user: Partial<T>) {
+    async function addIfNotExists<T>(table: string, criteria: Partial<T>, data: Partial<T> | (() => Promise<Partial<T>>)) {
+        const promised = typeof data === "function" ? await data() : data
         const exists = await knex(table).where(criteria).first()
         if (!exists)
-            return knex(table).insert(user)
+            return knex(table).insert(promised)
     }
 
     return Promise.all([
